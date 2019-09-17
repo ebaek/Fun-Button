591f72bd7d5da066cc3dbc81d8d68adc7d7f667e
Esther Baek <esther.baek5@gmail.com>
2019-09-16 22:25:48 -0400

    transitions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            buttonDisabled: true,
+        };
+
+        this.change = this.change.bind(this);
+    }
+
+    change(event) {
+        if (event.currentTarget.tagName !== "BUTTON") {
+            this.setState({
+                buttonDisabled: !this.state.buttonDisabled,
+            });
+        };
+    }
+
+    render() {
+        let text = this.state.buttonDisabled ? "Click Me" : "You Figured It Out";
+        let backgroundColor = this.state.buttonDisabled ? "main-container" : "change";
+
+        return (
+            <div className={backgroundColor} onClick={this.change}>
+                <button disabled={this.state.buttonDisabled}>{text}</button>
+            </div>
+        );
+    }
+}
+
+// React 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
